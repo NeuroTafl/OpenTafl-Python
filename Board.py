8748bcada6be760212fa710cefd1c989872852d4
drewbog <drew@bnimble.org>
2022-06-27 10:58:40 -0700

    More montecarlo work

---
+++ Board.py
@@ -0,0 +1,40 @@
+from Coordinate import Coordinate
+
+
+class Board:
+
+    board = []
+    currentBoardState = ""
+
+    def __init__(self, boardState):
+        self.currentBoardState = boardState
+        self.updateBoard()
+
+    def updateBoard(self) -> None:
+        rows = self.currentBoardState.split(sep="/")
+        array = []
+        for row in rows:
+            new_row = []
+            for piece in row:
+                self.log.debug(piece)
+                if piece.__contains__("t"):
+                    new_row.append("t")
+                elif piece.__contains__("T"):
+                    new_row.append("T")
+                elif piece.__contains__("K"):
+                    new_row.append("K")
+                else:
+                    spaces = int(piece)
+                    for x in range(spaces):
+                        new_row.append("e")
+            if len(new_row) > 0:
+                array.append(new_row)
+        array.reverse()
+        self.board = array
+
+    def checkCoord(self, coord: Coordinate) -> bool:
+        if self.board[coord.y][coord.x] == "e":
+            return True
+        return False
+
+
--- MoveDecider.py
+++ MoveDecider.py
@@ -1,16 +1,57 @@
+from Coordinate import Coordinate
+from Board import Board
+
 class MoveDecider:
 
-    board = []
+    board = None
 
     def decideMove(self, board, side):
-        self.board = board
+        self.board = Board(board)
         moves = self.generateAllPossible(side)
 
     def generateAllPossible(self, side) -> list:
 
-        # for every tile
-            # if correct piece
-            # for each direction
-                # add to list of places to go
-                # give score (monty carlo will be all score 1)
-        # return list
+        check = ""
+        if side == "attacker":
+            check = "T"
+        else:
+            check = "t"
+
+        possible = []
+        for y in range(len(Board.board)):
+            for x in range(len(Board.board[y])):
+                if Board.board[y][x] == check:
+                    coord = Coordinate(x=x, y=y)
+                    moveList = self.possibleMoves(coord)
+                    for move in moveList:
+                        possible.append(move)
+
+        return possible
+
+    def possibleMoves(self, coord: Coordinate) -> list:
+        start = coord.__str__()
+        moves = []
+        new_coord = Coordinate(x=coord.x - 1, y=coord.y)
+        # left
+        while(self.board.checkCoord(new_coord)):
+            moves.append(start + " " + new_coord.__str__())
+            new_coord.x = new_coord.x - 1
+
+        new_coord = Coordinate(x=coord.x + 1, y=coord.y)
+        # right
+        while (self.board.checkCoord(new_coord)):
+            moves.append(start + " " + new_coord.__str__())
+            new_coord.x = new_coord.x + 1
+
+        new_coord = Coordinate(x=coord.x, y=coord.y - 1)
+        # up
+        while (self.board.checkCoord(new_coord)):
+            moves.append(start + " " + new_coord.__str__())
+            new_coord.x = new_coord.y - 1
+
+        new_coord = Coordinate(x=coord.x, y=coord.y + 1)
+        # down
+        while (self.board.checkCoord(new_coord)):
+            moves.append(start + " " + new_coord.__str__())
+            new_coord.x = new_coord.y + 1
+
