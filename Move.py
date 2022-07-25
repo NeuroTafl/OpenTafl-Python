1177ae34303b900f8d4170fdbe0b90146fd934f2
Aaron S. Crandall <crandall@gonzaga.edu>
2022-07-25 14:35:31 -0700

    Starting to add full spec parsing/handling to Move class

--- Move.py
+++ Move.py
@@ -7,12 +7,22 @@ class Move:
         startingCoordinate: Coordinate = None,
         endingCoordinate: Coordinate = None,
         taflNotation: str = None,
+        openTaflNotation: str = None
     ):
         self.startingCoordinate = startingCoordinate
         self.endingCoordinate = endingCoordinate
 
+        self.taflmanSymbol = ""
+        self.startSpace = ""
+        self.endSpace = ""
+        self.moveCaptures = []
+        self.infoSymbol = ""
+
         if taflNotation:
             self.loadChessNotation(taflNotation)
+
+        if openTaflNotation:
+            self.loadFromOpenTaflNotation(openTaflNotation)
 
     def loadChessNotation(self, chessNotation: str) -> None:
         # TODO: This notation parsing is *way* too simple
@@ -21,6 +31,17 @@ class Move:
         (startCoordinate, endCoordinate) = chessNotation.split("-", 1)
         self.startingCoordinate = Coordinate(coordinate=startCoordinate)
         self.endingCoordinate = Coordinate(coordinate=endCoordinate)
+
+    def loadFromOpenTaflNotation(self, openTaflNotation: str) -> None:
+        # TODO: This notation parsing is *way* too simple
+        # Needs to handle kings, wins/loss, check, captures
+        # See: https://soapbox.manywords.press/2016/01/27/tafl-opentafl-notation/
+        # https://github.com/jslater89/OpenTafl/blob/master/opentafl-notation-spec.txt
+
+        (startCoordinate, endCoordinate) = openTaflNotation.split("-", 1)
+        self.startingCoordinate = Coordinate(coordinate=startCoordinate)
+        self.endingCoordinate = Coordinate(coordinate=endCoordinate)
+
 
     def __str__(self) -> str:
         return f"{str(self.startingCoordinate)}-{str(self.endingCoordinate)}"
--- test_Move.py
+++ test_Move.py
@@ -1,18 +1,34 @@
+#
+#
+# Move notation documentation:
+#  https://github.com/jslater89/OpenTafl/blob/master/opentafl-notation-spec.txt
+#
+# This is the standard OTN move record:
+#  [taflman-symbol]<starting-space><move-type><ending-space>[capture-record][info-symbol]
+#
+# Alternative (unsupported so far for OpenTafl-Python) - Damian Walker's notation:
+#  <starting-space><"-"><ending-space>[capture-record][info-symbol]
+# 
+
+
 from Move import Move
-# from Coordinate import Coordinate
+
+t_simpleMove_b2c2 = "b2-c2" # Taflman moves from b2 to c2
+t_simpleMove_j5j9 = "j5-j9" # Taflman moves from j5 to j9
+t_kingMove_h6b6 = "Kh6-b6"  # King moves from h6 to b6
 
 
 # ****************************************************************************
 # ****************************************************************************
 def test_MoveToString():
-    moveString = "b2-c2"
-    move = Move(taflNotation=moveString)
+    moveString = t_simpleMove_b2c2
+    move = Move(openTaflNotation=moveString)
     assert str(move) == moveString
 
 
 def test_MoveTestIndicies():
-    moveString = "j5-j9"
-    move = Move(taflNotation=moveString)
+    moveString = t_simpleMove_j5j9
+    move = Move(openTaflNotation=moveString)
 
     startXindex = 9
     startYindex = 4
@@ -23,3 +39,9 @@ def test_MoveTestIndicies():
     assert move.startingCoordinate.getYIndex() == startYindex
     assert move.endingCoordinate.getXIndex() == endXindex
     assert move.endingCoordinate.getYIndex() == endYindex
+
+def test_MoveKingSimple_h6b6():
+    moveString = t_kingMove_h6b6
+    #move = Move(openTaflNotation=moveString)
+
+
