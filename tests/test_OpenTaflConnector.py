858261fd4e1601ad407bb61ffeb5e17d20b1506f
Aaron S. Crandall <crandall@gonzaga.edu>
2022-08-03 17:08:13 -0700

    Moved tests to tests/ directory for proper structure (#18)
    * Initial OpenTaflConnector overhaul
    
    * Continuing to build out OpenTaflHandler and related components
    
    * Continued work towards handling state updates
    
    * Continued TaflRules development - almost ready to wire into TaflGame
    
    * Massive updates for Rules handling
    
    * Massive module movement and overhaul to incorporate Board tools into NeuroTafl agent directly
    
    * Overhaul for Board state storage and testing.
    
    * Moved tests to proper tests/ directory for cleanliness

---
+++ tests/test_OpenTaflConnector.py
@@ -0,0 +1,46 @@
+import pytest
+import sys
+from io import StringIO
+import logging
+from time import sleep
+
+from NeuroTaflAgent.OpenTaflConnector import OpenTaflConnector
+
+g_message = ""
+
+
+def messageHandler(message: str) -> None:
+    global g_message
+    g_message = message
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+# **************************************************************************#
+def test_BasicMessageHandlingWorking():
+    global g_message
+    expectedMessage = "abc"
+    sys.stdin = StringIO(expectedMessage)
+
+    connector = OpenTaflConnector("Test Connector")
+    connector.registerMessageCallbackHandler(messageHandler)
+    connector.run()
+    sleep(0.1)  # Must wait for connector thread to handle I/O
+
+    assert g_message == expectedMessage
+
+
+def test_HelloText(capsys):
+    connector = OpenTaflConnector("Test Connector")
+    connector.sendHello()
+    out, err = capsys.readouterr()
+    assert "hello" in out
+
+
+def test_InitText(capsys):
+    connector = OpenTaflConnector("Test Connector")
+    connector.init()
+    out, err = capsys.readouterr()
+    assert "hello" in out
+    assert "status" in out
+    assert "Test Connector" in out
